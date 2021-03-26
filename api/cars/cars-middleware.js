5c894c1ff19aac2ec3f94e2fa1d7724df9b96a2c
ngwatso <ngwatson79@yahoo.com>
2021-03-26 18:54:52 -0400

    set up server

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,57 +1,77 @@
 const Cars = require('./cars-model');
 var vinValidator = require('vin-validator');
 
-
-async const checkCarId = (req, res, next) => {
+const checkCarId = async (req, res, next) => {
 	// !! DO YOUR MAGIC
-  try {
-    const car = await Cars.getById(req.params.id)
-    if (car) {
-      req.car = car
-      next()
-    } else {
-      next({ ...Error(), status: 404, message: `Car with ID ${req.car.id} is not found`})
-    }
-  }
-  catch (err) {
-    next({ ...err, status: 500, message: 'Error processing request'})
-  }
+	try {
+		const car = await Cars.getById(req.params.id);
+		if (car) {
+			req.car = car;
+			next();
+		} else {
+			next({
+				...Error(),
+				status: 404,
+				message: `Car with ID ${req.params.id} is not found`,
+			});
+		}
+	} catch (err) {
+		next({ ...err, status: 500, message: 'Error processing request' });
+	}
 };
 
 const checkCarPayload = (req, res, next) => {
 	// !! DO YOUR MAGIC
-    const newCar = req.body
-    if ( !newCar.vin) {
-      next({ ...Error(), status: 400, message: `Car vin number is missing`})
-    } else if (!newCar.make) {
-      next({ ...Error(), status: 400, message: `Car make is missing`})
-    } else if (!newCar.model) {
-      next({ ...Error(), status: 400, message: `Car model is missing`})
-    } else if(!newCar.milage) {
-      next({ ...Error(), status: 400, message: `Car milage is missing`})
-    } else {
-      res.status(200).json(newCar)
-      next()
-    }
-  };
+	const newCar = req.body;
+	if (!newCar.vin) {
+		next({
+			...Error(),
+			status: 400,
+			message: `Car vin number is missing`,
+		});
+	} else if (!newCar.make) {
+		next({ ...Error(), status: 400, message: `Car make is missing` });
+	} else if (!newCar.model) {
+		next({ ...Error(), status: 400, message: `Car model is missing` });
+	} else if (!newCar.milage) {
+		next({ ...Error(), status: 400, message: `Car milage is missing` });
+	} else {
+		res.status(200).json(newCar);
+		next();
+	}
+};
 
 const checkVinNumberValid = (req, res, next) => {
 	// !! DO YOUR MAGIC
-  var isValidVin = vinValidator.validate(`${req.params.vin}`); 
-  if (isValidVin === false) {
-    next({ ...Error(), status: 400, message: `vin ${req.params.vin} is invalid`})
-  } else {
-    next()
-  }
-  }
-
+	var isValidVin = vinValidator.validate(`${req.params.vin}`);
+	if (isValidVin === false) {
+		next({
+			...Error(),
+			status: 400,
+			message: `vin ${req.params.vin} is invalid`,
+		});
+	} else {
+		next();
+	}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
 	// !! DO YOUR MAGIC
-  const vin = Cars.getAll(req.params.vin)
-  if(vin) {
-    next({ ...Error, status: 400, messgage: `vin ${req.params.vin} already exists`})
-  } else {
-    next()
-  }
+	const vin = Cars.getAll(req.params.vin);
+	if (vin) {
+		next({
+			...Error,
+			status: 400,
+			messgage: `vin ${req.params.vin} already exists`,
+		});
+	} else {
+		next();
+	}
+};
+
+module.exports = {
+	checkCarId,
+	checkCarPayload,
+	checkVinNumberValid,
+	checkVinNumberUnique,
 };
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,12 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+
+const router = express.Router();
+
+// !! DO YOUR MAGIC
+
+// ?? GET ==> /api/cars ==> Return array of cars
+
+// ?? GET ==> /api/cars/:id ==> Return car by given id
+
+// ?? POST ==> /api/cars ==> Return created car ==> Leading or trailing whitespace on name trimmed
