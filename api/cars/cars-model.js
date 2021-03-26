21260ca67c6bb35a9ca9662e0cef5aac134ad1e4
ngwatso <ngwatson79@yahoo.com>
2021-03-26 17:25:32 -0400

    completed database schema, cars-model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,18 @@
+const { insert } = require('../../data/db-config');
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+	// !! DO YOUR MAGIC
+	return db('cars');
+};
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getById = (id) => {
+	// !! DO YOUR MAGIC
+	return db('cars').first('*').where({ id });
+};
 
-const create = () => {
-  // DO YOUR MAGIC
+async const create = (newCar) => {
+	// !! DO YOUR MAGIC
+const cars = await db('cars').insert(newCar)
+return getById(cars[0])
 }
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+// !! DO YOUR MAGIC
+
+exports.up = function (knex) {
+	return knex.schema.crfeateTable('cars', (tbl) => {
+		tbl.increments();
+		tbl.string('vin').unique().notNullable();
+		tbl.string('make').notNullable();
+		tbl.string('model').notNullable();
+		tbl.integer('milage').notNullable();
+		tbl.string('title');
+		tbl.string('transmission');
+	});
+};
+
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists('cars');
+};
